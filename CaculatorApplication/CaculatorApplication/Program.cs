6a5d3cd4bdc1f8f18dfd9363fa5e9ef2d541e2a7
Prasanti Kadiyala <prasanti.kadiyala@tcs.com>
2016-07-04 09:41:41 +0000

    Calculator Solution

---
+++ CaculatorApplication/CaculatorApplication/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaculatorApplication
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+      var result = MathLib.Math.Add(1, 2);
+      Console.WriteLine("The result is {0}", result);
+    }
+  }
+}
---
+++ CaculatorApplication/MathLib/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLib
+{
+    public static class Math
+    {
+      public static int Add(int a, int b)
+      {
+        var result = a + b;
+        return result ;
+      }      
+    }
+}
