0fb138879c968e2fefc7f5ddb7602649be005e02
Prasanti Kadiyala <prasanti.kadiyala@tcs.com>
2016-07-04 10:14:05 +0000

    Commit for Subtraction Method

--- CaculatorApplication/MathLib/Math.cs
+++ CaculatorApplication/MathLib/Math.cs
@@ -8,15 +8,44 @@ namespace MathLib
 {
     public static class Math
     {
+      /// <summary>
+      /// Addition method
+      /// </summary>
+      /// <param name="a"></param>
+      /// <param name="b"></param>
+      /// <returns></returns>
       public static int Add(int a, int b)
       {
         var result = a + b;
         return result ;
       }      
+
+      /// <summary>
+      ///  Multiplication method
+      /// </summary>
+      /// <param name="a"></param>
+      /// <param name="b"></param>
+      /// <returns></returns>
       public static int Mul(int a , int b)
       {
         var result = a * b;
         return result;
+      }
+
+      /// <summary>
+      /// Subtraction method
+      /// </summary>
+      /// <param name="a"></param>
+      /// <param name="b"></param>
+      /// <returns></returns>
+      public static int Sub(int a , int b)
+      {
+        int result;
+        if (a > b)
+          result = a - b;
+        else
+          result = b - a;
+        return result;        
       }
     }
 }
