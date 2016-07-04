0f3d97879aa448c99f1ad3f75070a26b9a0d8841
Prasanti Kadiyala <prasanti.kadiyala@tcs.com>
2016-07-04 10:38:42 +0000

    Added comments

---
+++ CaculatorApplication/CaculatorApplication/Calc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaculatorApplication
+{
+  public class Calc
+  {
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public int Calc(int a , int b)
+    {
+      var result = a + b;
+      return result;
+    }
+
+  }
+}
